4832968db1e3fa209bc1972802fa610f64a40854
Cephalon0w0 <victtorbadillo@hotmail.com>
2025-02-26 13:36:45 -0600

    Update BG

--- Tareas/VMBG/Satisfaction.py
+++ Tareas/VMBG/Satisfaction.py
@@ -6,17 +6,32 @@ def GetSatisfaction(Vmin, Vmax, Vo, isMaximization):
 
 
 def GetInputVector():
-    inputVector = input("Ingresa el vector solucion T/H/R/IL\n").strip().split(',')
-
+    inputVector = input("Ingresa el vector solucion T/H/R/IL\n")
+    if inputVector == 'exit':
+        exit('Program Terminated')
+    else:
+        inputVector = inputVector.strip().split(',')
     if len(inputVector) != 4:
         exit("Vector Invalido")
     try:
+        print('Parseando vector...')
         for i in range(len(inputVector)):
             inputVector[i] = int(inputVector[i])
     except ValueError:
         exit("Valores invalidos")
 
     return inputVector
+
+
+def GetMinimizationEnergyCost(cost, currentValue, desiredValue):
+    if currentValue <= desiredValue:
+        return 0
+    else:
+        return cost + cost * (currentValue - desiredValue)
+
+
+def GetMaximizationEnergyCost(cost, currentValue, desiredValue):
+    return 1
 
 
 if __name__ == '__main__':
@@ -45,16 +60,44 @@ if __name__ == '__main__':
         True
     ]
 
-    while True:
-        solutions = GetInputVector()
+    costPerUnitOfChange = [
+        40,
+        50,
+        60,
+        70
+    ]
 
-        satisfaction = []
+    #solutions = GetInputVector()
+    currentValues = [23, 65, 56, 98]
+    solutions = [20, 55, 12, 252]
 
-        i = 0
-        for j in range(len(weights)):
-            satisfaction.append(GetSatisfaction(ranges[i], ranges[i + 1], solutions[j], maximization[j]))
-            i += 2
+    satisfaction = []
 
-        satisfaction = [satisfaction[i] * weights[i] for i in range(len(weights))]
+    i = 0
+    print("Calculando satisfaccion de servicios...")
+    for j in range(len(weights)):
+        satisfaction.append(GetSatisfaction(ranges[i], ranges[i + 1], solutions[j], maximization[j]))
+        i += 2
 
-        print(f'Satisfaccion Total = {sum(satisfaction)}\n')
+    print("Calculando satisfaccion total...")
+    satisfaction = [satisfaction[i] * weights[i] for i in range(len(weights))]
+
+    print(f'Satisfaccion Total = {sum(satisfaction)}\n')
+
+    energyCosts = []
+    rangesIndex = 0
+    for i in range(len(costPerUnitOfChange)):
+        if maximization[i]:
+            pass
+            # energyPredicted = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], solutions[i])
+            # energyMin = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex])
+            # energyMax = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex + 1])
+        else:
+            energyPredicted = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], solutions[i])
+            energyMax = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex])
+            energyMin = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex + 1])
+            energyCosts.append(round(1 - (energyMax - energyPredicted) / (energyMax - energyMin), 2))
+        rangesIndex += 2
+        # energyCosts.append(round(1 - (energyMax - energyPredicted) / (energyMax - energyMin), 2))
+    print('Costos de Energia')
+    print(energyCosts)
