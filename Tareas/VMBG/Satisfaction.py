4b7df3e192448c82f3af3906f4db3d37c44917c0
Cephalon0w0 <victtorbadillo@hotmail.com>
2025-03-02 17:03:27 -0600

    Update BG

--- Tareas/VMBG/Satisfaction.py
+++ Tareas/VMBG/Satisfaction.py
@@ -24,26 +24,25 @@ def GetInputVector():
 
 
 def GetMinimizationEnergyCost(cost, currentValue, desiredValue):
-    if currentValue <= desiredValue:
-        return 0
-    else:
-        return cost + cost * (currentValue - desiredValue)
+    return cost + cost * (currentValue - desiredValue) if currentValue >= desiredValue else 0
 
 
 def GetMaximizationEnergyCost(cost, currentValue, desiredValue):
-    return 1
+    return cost + cost * (desiredValue - currentValue) if currentValue <= desiredValue else 0
 
 
 if __name__ == '__main__':
+    alpha = 0.5
+    beta = 0.5
     ranges = [
-        16,     # TMin
-        21,     # TMax
-        40,     # HMin
-        60,     # HMax
-        0,      # NMin
-        60,     # NMax
-        100,    # LMin
-        300     # LMax
+        16,   # TMin
+        21,   # TMax
+        40,   # HMin
+        60,   # HMax
+        0,    # NMin
+        60,   # NMax
+        100,  # LMin
+        300   # LMax
     ]
 
     weights = [
@@ -61,13 +60,13 @@ if __name__ == '__main__':
     ]
 
     costPerUnitOfChange = [
-        40,
+        25,
+        30,
         50,
-        60,
-        70
+        5
     ]
 
-    #solutions = GetInputVector()
+    # solutions = GetInputVector()
     currentValues = [23, 65, 56, 98]
     solutions = [20, 55, 12, 252]
 
@@ -82,22 +81,25 @@ if __name__ == '__main__':
     print("Calculando satisfaccion total...")
     satisfaction = [satisfaction[i] * weights[i] for i in range(len(weights))]
 
-    print(f'Satisfaccion Total = {sum(satisfaction)}\n')
+    print(f'Satisfaccion Total = {round(sum(satisfaction), 5)}\n')
 
     energyCosts = []
+    gain = 0
     rangesIndex = 0
     for i in range(len(costPerUnitOfChange)):
         if maximization[i]:
             pass
-            # energyPredicted = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], solutions[i])
-            # energyMin = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex])
-            # energyMax = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex + 1])
+            energyPredicted = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], solutions[i])
+            energyMin = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex])
+            energyMax = GetMaximizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex + 1])
         else:
             energyPredicted = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], solutions[i])
             energyMax = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex])
             energyMin = GetMinimizationEnergyCost(costPerUnitOfChange[i], currentValues[i], ranges[rangesIndex + 1])
-            energyCosts.append(round(1 - (energyMax - energyPredicted) / (energyMax - energyMin), 2))
         rangesIndex += 2
-        # energyCosts.append(round(1 - (energyMax - energyPredicted) / (energyMax - energyMin), 2))
-    print('Costos de Energia')
-    print(energyCosts)
+        energyCosts.append(round(1 - (energyMax - energyPredicted) / (energyMax - energyMin), 5))
+        gain += energyCosts[i] * weights[i]
+    print('Costos de Energia', energyCosts)
+    print('Ganancia:', round(gain, 5))
+    fo = round(sum(satisfaction) * alpha + gain * beta, 5)
+    print('Fo:', fo)
